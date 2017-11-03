dc648171db0637342725f4b8068c12db22807399
Robert Lehmann <robert.lehmann@sitec-systems.de>
2017-11-03 13:09:30 +0100

    FindLibsocketcan: Move cmake script into the main cmake dir
    Ticket #1951

--- cmake/FindLibsocketcan.cmake
+++ cmake/FindLibsocketcan.cmake
@@ -1,3 +1,7 @@
+# Copyright 2017 sitec systems GmbH. All rights reserved
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
 # - Try to find libsocketcan
 # Once done it will define
 #  LIBSOCKETCAN_FOUND - System has libsocketcan
